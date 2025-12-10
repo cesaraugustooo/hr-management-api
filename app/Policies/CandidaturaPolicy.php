af9e33e17dd564cfba0a1c48002a29f5fe00fa3a
Cesar Augusto <cesaraugusto23121989@gmail.com>
2025-12-09 23:03:45 -0300

    feat: Adiciona policie as candidaturas

---
+++ app/Policies/CandidaturaPolicy.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Candidatura;
+use App\Models\Empresa;
+use App\Models\User;
+use App\Models\Vaga;
+use Illuminate\Auth\Access\Response;
+
+use function PHPUnit\Framework\returnArgument;
+
+class CandidaturaPolicy
+{
+    /**
+     * Determine whether the user can view any models.
+     */
+    public function viewAny(User $user): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(User $user, Candidatura $candidatura): bool
+    {
+        $empresa = $candidatura->vaga->empresa;
+
+        if($empresa->user_admin == $user->id){
+            return true;
+        }
+
+        foreach($empresa->companyAssociation as $user_company){
+            if($user_company->id == $user->id){
+                return true;
+            } 
+        }
+
+        return $user->id == $candidatura->users_id;
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(User $user): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, Candidatura $candidatura): bool
+    {
+        return $user->id == $candidatura->users_id;
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     */
+    public function restore(User $user, Candidatura $candidatura): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     */
+    public function forceDelete(User $user, Candidatura $candidatura): bool
+    {
+        return false;
+    }
+
+     public function updateCandidaturaStatus(User $user, Candidatura $candidatura): bool
+    {
+        $empresa = $candidatura->vaga->empresa;
+
+        if($empresa->user_admin == $user->id){
+            return true;
+        }
+
+        foreach($empresa->companyAssociation as $user_company){
+            if($user_company->id == $user->id){
+                return true;
+            } 
+        }
+        return false;
+    }
+}
