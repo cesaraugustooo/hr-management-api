3cb55301f8102dca38c65634bc0484a67e70f973
Cesar Augusto <cesaraugusto23121989@gmail.com>
2026-01-07 21:48:05 -0300

    refactor: centraliza lógica de negócio no service

--- app/services/EmpresaService.php
+++ app/services/EmpresaService.php
@@ -5,9 +5,48 @@ namespace App\services;
 use App\Http\Requests\AssociationCompanyRequest;
 use App\Models\Empresa;
 use Exception;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
 class EmpresaService
 {
+    use AuthorizesRequests;
+
+    public function index(): object
+    {
+        $empresas = Empresa::with(['companyAssociation'])->paginate();
+
+        return $empresas;
+    }
+
+    
+    public function store($data): Empresa
+    {
+        $empresa = Empresa::create($data);
+
+        return $empresa;
+    }
+
+    
+    public function update($data,$empresa): Empresa
+    {
+        $this->authorize("update",$empresa);
+
+        $empresa->update($data);
+
+        return $empresa;
+    }
+
+    
+    public function destroy($empresa)
+    {
+        $this->authorize("update",$empresa);
+
+        $empresa->delete();
+
+        return true;
+    }
+
+
     public function associationCompanyHasUser($arrayUsers,Empresa $empresa){
         try {            
             $empresa->companyAssociation()->attach($arrayUsers);
