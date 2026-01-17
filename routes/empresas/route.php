62b56e995ce847ad4298788754e2aa4a7826abc6
cesaraugustooo <cesaraugusto23121989@gmail.com>
2026-01-17 15:09:41 -0300

    feat: adiciona feat para ver informações do seu perfil

--- routes/empresas/route.php
+++ routes/empresas/route.php
@@ -1,7 +1,12 @@
 <?php
 
 use App\Http\Controllers\Api\EmpresaController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+Route::get('/me',function(Request $request){
+    return $request->user()->load("empresaAdmin");
+})->middleware('cookie-auth');
 
 Route::apiResource('empresas', EmpresaController::class);
 Route::post('/assoc-users/{empresa}',[EmpresaController::class, 'associationUser']);
